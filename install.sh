8cf39c643664cf55d45d13d3655d41ec9af2263e
Peter Willis <pjw7904@rit.edu>
2024-11-19 11:22:09 -0500

    Remove sudo from install script

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 # INSTALL BASE DEPENDENCIES
 echo "Installing Python and curl..."
 
-sudo apt update & sudo apt install -y python3 python3-pip curl
+apt update & apt install -y python3 python3-pip curl
 ############################################
 
 ############################################
 # INSTALL MININET
 echo "Installing Mininet..."
 
-sudo apt install mininet
+apt install mininet
 ############################################
 
 ############################################
@@ -20,18 +20,18 @@ sudo apt install mininet
 echo "Installing FRR..."
 
 ## add GPG key
-curl -s https://deb.frrouting.org/frr/keys.gpg | sudo tee /usr/share/keyrings/frrouting.gpg > /dev/null
+curl -s https://deb.frrouting.org/frr/keys.gpg | tee /usr/share/keyrings/frrouting.gpg > /dev/null
 
 ## frr-stable will be the latest official stable release
 FRRVER="frr-stable"
 echo deb '[signed-by=/usr/share/keyrings/frrouting.gpg]' https://deb.frrouting.org/frr \
-     $(lsb_release -s -c) $FRRVER | sudo tee -a /etc/apt/sources.list.d/frr.list
+     $(lsb_release -s -c) $FRRVER | tee -a /etc/apt/sources.list.d/frr.list
 
 ## update and install FRR
-sudo apt update && sudo apt install -y frr frr-pythontools
+apt update && apt install -y frr frr-pythontools
 
 # Give permissions to user to access frr files (this requires a logout after to take effect)
-sudo usermod -a -G frr,frrvty $(logname)
+usermod -a -G frr,frrvty $(logname)
 ############################################
 
 ############################################
@@ -52,7 +52,7 @@ fi
 # Assuming possible use of networking libraries and logging
 echo "Installing C development tools..."
 
-sudo apt-get install -y build-essential autoconf automake libtool bison flex gdb cmake pkg-config libpcap-dev
+apt install -y build-essential autoconf automake libtool bison flex gdb cmake pkg-config libpcap-dev
 
 # Compile MTP source files
 MTP_SRC_DIR="closnet/switches/mtp/src"
@@ -60,7 +60,7 @@ MTP_BIN="closnet/switches/mtp/bin/mtp"
 
 if [ -d "$MTP_SRC_DIR" ]; then
     echo "Compiling MTP source files..."
-    sudo gcc $MTP_SRC_DIR/*.c -o $MTP_BIN
+    gcc $MTP_SRC_DIR/*.c -o $MTP_BIN
 else
     echo "MTP implementation source directory not found, skipping compilation."
 fi
