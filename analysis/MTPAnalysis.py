56a84744aba6e0c5abe4b01b1721e99455898d00
Peter Willis <pjw7904@rit.edu>
2025-02-25 21:35:44 -0500

    Refactored MTPAnalysis away from its Jupyter roots

--- analysis/MTPAnalysis.py
+++ analysis/MTPAnalysis.py
@@ -1,8 +1,17 @@
-LOG_DIR_PATH = "/home/pjw7904/MTP-Mininet/logs/mtp/mtp_2_4_1-1_1739380399451"
+import os
+import re
+
+# Topology and experiment information
+LOG_DIR_PATH = "/home/pjw7904/MTP-Mininet/logs/mtp/mtp_2_4_1-1_1740497425069"
+DOWNTIME_DIR = "downtime"
+CONVERGENCE_DIR = "convergence"
 NETWORK_NODE_PREFIXES = "T,S,L"
 COMPUTE_NODE_PREFIXES = "C"
 
-import os
+# Types values for failure actions
+FAILURE_DETECTION = 0
+FAILURE_UPDATE = 1
+
 
 def getResultsFile(metricDirectory):
     directoryPath = os.path.join(LOG_DIR_PATH, metricDirectory)
@@ -17,40 +26,13 @@ def getResultsFile(metricDirectory):
     return
 
 
-def getIntfDownTime(logFile):
+def getDownTime(logFile):
     '''
-    Read the first line of the interface downtime file, which must be the time in epoch formatting.
+    Read the first line of a downtime file, which must be the time in epoch formatting.
     '''
 
     with open(logFile) as file:
         return int(file.readline())
-
-
-def getTestStopTime(logFile):
-    '''
-    Find the stoptime for each node and determine the one that stopped first. That time is the end of the test.
-    '''
-    testStopTime = 0
-    
-    with open(logFile) as file:
-        lines = file.readlines()
-
-        for line in lines:
-            nodeStopTime = int(line.split(":")[1])
-            testStopTime = nodeStopTime if nodeStopTime < testStopTime or testStopTime == 0 else testStopTime
-    
-    return testStopTime
-
-# Analyze each file in the downtime subdirectory
-for logFile in getResultsFile("downtime"):
-    if("nodes_down" in logFile): # Determine what the test stoptime was.
-        testStopTime = getTestStopTime(logFile)
-    elif("intf_down" in logFile): # Determine when the interface failure occurred.
-        failureTime = getIntfDownTime(logFile)
-    else:
-        print(f"Unknown file {logFile}")
-
-print(f"Interface failure timestamp: {failureTime}\nTest stop timestamp: {testStopTime}")
 
 
 def parseTimestamp(line):
@@ -59,6 +41,7 @@ def parseTimestamp(line):
     '''
     return int(line.split(" ")[6].replace(",",""))
 
+
 def parseOverhead(line, timestamp):
     '''
     Get the size of the failure message.
@@ -66,6 +49,7 @@ def parseOverhead(line, timestamp):
     overhead = int(line.split("=")[1]) if timestamp < testStopTime else 0
 
     return overhead
+
 
 def getOverhead(logFile):
     with open(logFile) as f:
@@ -81,6 +65,42 @@ def getOverhead(logFile):
 
     return size
 
+
+def getNodeConvergenceTime(logFile):
+    with open(logFile) as f:
+        line = f.readline()
+    
+        while line:
+            token = line.split(" ")
+            
+            if "Detected a failure" in line:            
+                return FAILURE_DETECTION, int(token[len(token)-1])
+            
+            elif "FAILURE UPDATE message received" in line:
+                time = int(token[6].replace(",",""))
+    
+                if(time < testStopTime):
+                    return FAILURE_UPDATE, time
+            
+            line = f.readline()
+        
+    return -1,-1
+
+
+# STEP 1: Parse and record timestamps for downtime (interface, network)
+for logFile in getResultsFile(DOWNTIME_DIR):
+    if("experiment_stop" in logFile): # Determine what the test stoptime was.
+        testStopTime = getDownTime(logFile)
+
+    elif("intf_down" in logFile): # Determine when the interface failure occurred.
+        failureTime = getDownTime(logFile)
+    
+    else:
+        print(f"Unknown file {logFile}")
+
+print(f"Interface failure timestamp: {failureTime}\nTest stop timestamp: {testStopTime}")
+
+
 # Control Overhead values
 totalOverhead = 0
 
@@ -88,7 +108,7 @@ totalOverhead = 0
 totalNodeCount = 0
 effectedNodeCount = 2 # Starts at 2 for the 2 nodes that lost a link. They won't have any updates.
 
-for logFile in getResultsFile("convergence"):
+for logFile in getResultsFile(CONVERGENCE_DIR):
     # Blast radius value updates
     totalNodeCount += 1
     
@@ -112,35 +132,11 @@ print(f"Overhead: {totalOverhead} bytes")
 print(f"\nBlast radius: {blastRadius:.2f}% of nodes received VID failure information.")
 print(f"\tNodes receiving updated information: {effectedNodeCount}\n\tTotal nodes: {totalNodeCount}")
 
-# Types values for failure actions
-FAILURE_DETECTION = 0
-FAILURE_UPDATE = 1
-
-def getNodeConvergenceTime(logFile):
-    with open(logFile) as f:
-        line = f.readline()
-    
-        while line:
-            token = line.split(" ")
-            
-            if "Detected a failure" in line:            
-                return 0,int(token[len(token)-1])
-            
-            elif "FAILURE UPDATE message received" in line:
-                time = int(token[6].replace(",",""))
-    
-                if(time < testStopTime):
-                    return 1,int(time)
-            
-            line = f.readline()
-        
-    return -1,-1
-
 # Default starting values for the timing range
 failureDetectionTimestamp = -1
 finalFailureRecoveryTimestamp = -1
 
-for logFile in getResultsFile("convergence"):
+for logFile in getResultsFile(CONVERGENCE_DIR):
     if(os.path.getsize(logFile) > 0):
         failureType, failureTimestamp = getNodeConvergenceTime(logFile)
 
